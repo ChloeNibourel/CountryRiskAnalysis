4ccc089f70d62395f8e6a33103f00f30d469f322
Chloe Nibourel <chloe.nibourel@sciencespo.fr>
2016-05-12 15:53:04 +0200

    documentation main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+/// \file main.cpp
+/// \brief Choices: 1.Read 2.Update 3.Simulate 4.Exit
+/// User have 4 choices opening the program
+/// 1.Read some datas used to simulate
+/// 2.Update some datas to improve the simulation
+/// 3.Simulate different shocks 
+/// 4.Exit the program
+
+
