a490553ffa5c206529f1f8c2f6051b24ecd88e17
Chloe Nibourel <chloe.nibourel@sciencespo.fr>
2016-05-12 14:15:28 +0200

    better test

--- Country.h
+++ Country.h
@@ -1,10 +1,10 @@
-//test branching
+
 #include <iostream>
 #include <header.h>
 #include <string>
 
 class Country {
-	<std::string> Name;
+	std::string Name;
 	double GDP;
 	double ElasticityExport;
 	double ElasticityImport;
