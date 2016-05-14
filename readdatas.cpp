c9a3d2c28800ede15950f61931fa346a3dd87fc1
Chloe Nibourel <chloe.nibourel@sciencespo.fr>
2016-05-14 13:25:16 +0200

    Read Canada's datas

---
+++ readdatas.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+std::string read() {
+	int choice;
+	
+	std::cout << "Which country are you interested in ? 1/2/3" << std::endl;
+	std::cout << " 1. Canada" << std::endl;
+	std::cout << " 2. United States" << std::endl;
+	std::cout << " 3. Mexico" << std::endl;
+
+	std::cin>> choice;
+
+	if (choice==1) {
+		std::cout << "GDP of Canada is " << can.gdp << " Billion USD"<< std::endl;
+		std::cout << "Total export of Canada is " << can.exp << " Billion USD"<< std::endl;
+
+		std::cout << "Canada exports " << ????? << " Billion USD to the United States"<< std::endl;
+		std::cout << "  which include: " << std::endl;
+		std::cout << "   " << ????? << " Billion USD of mineral fuels"<< std::endl;
+		std::cout << "   " << ????? << " Billion USD of food"<< std::endl;
+		std::cout << "   " << ????? << " Billion USD of machinery and transport equipments"<< std::endl;
+
+		std::cout << "Canada exports " << ????? << " Billion USD to Mexico"<< std::endl;
+		std::cout << "  which include: " << std::endl;
+		std::cout << "   " << ????? << " Billion USD of mineral fuels"<< std::endl;
+		std::cout << "   " << ????? << " Billion USD of food"<< std::endl;
+		std::cout << "   " << ????? << " Billion USD of machinery and transport equipments"<< std::endl;
+	}
+}
