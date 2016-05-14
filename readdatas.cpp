56b517bec1807ec4b075bad62aa97cc62c8e9f50
Chloe Nibourel <chloe.nibourel@sciencespo.fr>
2016-05-14 13:35:16 +0200

    add US and Mexico's datas

--- readdatas.cpp
+++ readdatas.cpp
@@ -27,4 +27,42 @@ std::string read() {
 		std::cout << "   " << ????? << " Billion USD of food"<< std::endl;
 		std::cout << "   " << ????? << " Billion USD of machinery and transport equipments"<< std::endl;
 	}
+
+	else if (choice==2) {
+		std::cout << "GDP of the US is " << usa.gdp << " Billion USD"<< std::endl;
+		std::cout << "Total export of the US is " << usa.exp << " Billion USD"<< std::endl;
+
+		std::cout << "The US exports " << ????? << " Billion USD to Canada"<< std::endl;
+		std::cout << "  which include: " << std::endl;
+		std::cout << "   " << ????? << " Billion USD of mineral fuels"<< std::endl;
+		std::cout << "   " << ????? << " Billion USD of food"<< std::endl;
+		std::cout << "   " << ????? << " Billion USD of machinery and transport equipments"<< std::endl;
+
+		std::cout << "The US exports " << ????? << " Billion USD to Mexico"<< std::endl;
+		std::cout << "  which include: " << std::endl;
+		std::cout << "   " << ????? << " Billion USD of mineral fuels"<< std::endl;
+		std::cout << "   " << ????? << " Billion USD of food"<< std::endl;
+		std::cout << "   " << ????? << " Billion USD of machinery and transport equipments"<< std::endl;
+	}
+
+	else if (choice==3) {
+		std::cout << "GDP of Mexico is " << mex.gdp << " Billion USD"<< std::endl;
+		std::cout << "Total export of Mexico is " << mex.exp << " Billion USD"<< std::endl;
+
+		std::cout << "Mexico exports " << ????? << " Billion USD to Canada"<< std::endl;
+		std::cout << "  which include: " << std::endl;
+		std::cout << "   " << ????? << " Billion USD of mineral fuels"<< std::endl;
+		std::cout << "   " << ????? << " Billion USD of food"<< std::endl;
+		std::cout << "   " << ????? << " Billion USD of machinery and transport equipments"<< std::endl;
+
+		std::cout << "Mexico exports " << ????? << " Billion USD to the United States"<< std::endl;
+		std::cout << "  which include: " << std::endl;
+		std::cout << "   " << ????? << " Billion USD of mineral fuels"<< std::endl;
+		std::cout << "   " << ????? << " Billion USD of food"<< std::endl;
+		std::cout << "   " << ????? << " Billion USD of machinery and transport equipments"<< std::endl;
+	}
+
+	else { 
+		std::cout << "Error: choose 1, 2 or 3"<< std::endl;
+	}
 }
