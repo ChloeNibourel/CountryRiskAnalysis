ca9b5cdf94f9a7281b897e7a17c8bde53c5c7145
Chloe Nibourel <chloe.nibourel@sciencespo.fr>
2016-05-14 14:00:49 +0200

    add documentation

--- readdatas.cpp
+++ readdatas.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-#include <string>
 
-std::string read() {
+/// \file readdatas.cpp
+/// \brief show datas available for a given country
+
+/// function read() : choose a country to have its
+/// GDP, Total export, Export to other countries (total and by fields)
+/// Countries: Canada, USA, Mexico
+/// fields: Mineral fuels, Food, Machinery and transport equipments
+
+
+void read() {
 	int choice;
+
+//chose a country
 	
 	std::cout << "Which country are you interested in ? 1/2/3" << std::endl;
 	std::cout << " 1. Canada" << std::endl;
@@ -11,6 +21,7 @@ std::string read() {
 
 	std::cin>> choice;
 
+// Canada
 	if (choice==1) {
 		std::cout << "GDP of Canada is " << can.gdp << " Billion USD"<< std::endl;
 		std::cout << "Total export of Canada is " << can.exp << " Billion USD"<< std::endl;
@@ -28,6 +39,7 @@ std::string read() {
 		std::cout << "   " << ????? << " Billion USD of machinery and transport equipments"<< std::endl;
 	}
 
+//USA
 	else if (choice==2) {
 		std::cout << "GDP of the US is " << usa.gdp << " Billion USD"<< std::endl;
 		std::cout << "Total export of the US is " << usa.exp << " Billion USD"<< std::endl;
@@ -45,6 +57,7 @@ std::string read() {
 		std::cout << "   " << ????? << " Billion USD of machinery and transport equipments"<< std::endl;
 	}
 
+//Mexico
 	else if (choice==3) {
 		std::cout << "GDP of Mexico is " << mex.gdp << " Billion USD"<< std::endl;
 		std::cout << "Total export of Mexico is " << mex.exp << " Billion USD"<< std::endl;
@@ -62,6 +75,7 @@ std::string read() {
 		std::cout << "   " << ????? << " Billion USD of machinery and transport equipments"<< std::endl;
 	}
 
+//Wrong answer
 	else { 
 		std::cout << "Error: choose 1, 2 or 3"<< std::endl;
 	}
