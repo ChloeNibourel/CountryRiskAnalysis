8876ae227cb2f7be6c50226d92941a9dfa96c4f9
Marie.Cases <marie.cases@sciencespo.fr>
2016-05-15 09:15:21 +0200

    Added forgotten ;

--- country.h
+++ country.h
@@ -4,14 +4,14 @@
 class country {
 public: 
 	std::string name;
-	std::string othercountryexports;
-	float exchangerate /// \bug figure out how we want to read exchange rates.
-	float elasticityimports
-	float elasticityexports
-	float Income
-	float A
-	float totalimports
-	float totalexports
+	std::string othercountryexports; /// \bug figure out how we want to read exchange rates.
+	float elasticityimports;
+	float elasticityexports;
+	float Income;
+	float A;
+	float totalimports;
+	float totalexports;
+	std::string exportmarkets;
 
 	double GDP();
 	void GDPShockImpact ();
