4257d18284420f4c4078c64c422bb93a3908589b
Mylène_Feuillade <mylene.feuillade@sciencespo.fr>
2016-05-15 19:27:11 +0200

    modified functions signature

--- headers/getdata.h
+++ headers/getdata.h
@@ -1,3 +1,5 @@
-void getcountry(libxl::Book* book, libxl::Sheet* sheet);
+#include "../headers/country.h"
+void getdata (country* country1, country* country2, country* country3);
+void getcountry(libxl::Book* book, libxl::Sheet* sheet, country* mycountry);
 
 void getmarket (libxl::Book* book, libxl::Sheet* sheet);
