c23f1338d9a13e2c34310766fb6a3f737493859f
Mylène_Feuillade <mylene.feuillade@sciencespo.fr>
2016-05-15 02:30:48 +0200

    removed the libxl library from git

--- libxl-3.6.5.0/include_c/handle.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIBXL_HANDLE_H
-#define LIBXL_HANDLE_H
-
-typedef struct tagBookHandle * BookHandle;
-typedef struct tagSheetHandle * SheetHandle;
-typedef struct tagFormatHandle * FormatHandle;
-typedef struct tagFontHandle * FontHandle;
-
-#endif
