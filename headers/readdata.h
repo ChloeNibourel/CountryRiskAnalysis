18981de16e1dc3df0c8d10c0494624c7002d3691
Mylène_Feuillade <mylene.feuillade@sciencespo.fr>
2016-05-16 02:32:21 +0200

    New header to read data

---
+++ headers/readdata.h
@@ -0,0 +1,5 @@
+void read(country* country1, country* country2, country* country3, market* market1, market* market2, market* market3);
+
+void readcountry(country* mycountry);
+
+void readmarket(market* mymarket);
