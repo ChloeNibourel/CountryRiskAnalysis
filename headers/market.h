42a22871493fbef15e25392b73ff915939a16499
Mylène_Feuillade <mylene.feuillade@sciencespo.fr>
2016-05-16 19:09:36 +0200

    changed market class again

--- headers/market.h
+++ headers/market.h
@@ -9,36 +9,36 @@ class market{
 
 	/* 
 	Each component of the exchange array corresponds to tha data about total exports between two countries in the market :
-	exchange[1][1]= us -> us =0
-	exchange[1][2]= us -> canada
-	exchange[1][3]= us -> mexico
+	exchange[0][0]= us -> us =0
+	exchange[0][1]= us -> canada
+	exchange[0][2]= us -> mexico
 	
-	exchange[2][1]= canada -> us
-	exchange[2][2]= canada -> canada =0
-	exchange[2][3]= canada -> mexico
+	exchange[1][0]= canada -> us
+	exchange[1][1]= canada -> canada =0
+	exchange[1][2]= canada -> mexico
 
-	exchange[3][1]= mexico -> us
-	exchange[3][2]= mexico -> canada 
-	exchange[3][3]= mexico -> mexico =0	*/	
-	std::array<double, 6> exchange;	
+	exchange[2][0]= mexico -> us
+	exchange[2][1]= mexico -> canada 
+	exchange[2][1]= mexico -> mexico =0	*/
+	*/
+	std::array<std::array<double, 3>, 3> exchange;	
 };
 
 ///\class exportsnetwork
 ///\brief contains data about total exports between countries
 class exportsnetwork {
 	/*
-	exchange[1][1]= us -> us =0
-	exchange[1][2]= us -> canada
-	exchange[1][3]= us -> mexico
+	exports[0][0]= us -> us =0
+	exports[0][1]= us -> canada
+	exports[0][2]= us -> mexico
 	
-	exchange[2][1]= canada -> us
-	exchange[2][2]= canada -> canada =0
-	exchange[2][3]= canada -> mexico
+	exports[1][0]= canada -> us
+	exports[1][1]= canada -> canada =0
+	exports[1][2]= canada -> mexico
 
-	exchange[3][1]= mexico -> us
-	exchange[3][2]= mexico -> canada 
-	exchange[3][3]= mexico -> mexico =0
-	*/
+	exports[2][0]= mexico -> us
+	exports[2][1]= mexico -> canada 
+	exports[2][1]= mexico -> mexico =0	*/		*/
 	public:
 	std::array<std::array<double, 3>, 3> exports;
 };
