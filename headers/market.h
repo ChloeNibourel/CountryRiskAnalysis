178f8ddfbfbb76fe544095fcb03db55c3520f431
Mylène_Feuillade <mylene.feuillade@sciencespo.fr>
2016-05-16 18:55:51 +0200

    modified class market and explained class exportnetwork

--- headers/market.h
+++ headers/market.h
@@ -8,24 +8,37 @@ class market{
 	std::string good;
 
 	/* 
-	Each component of the exchange array corresponds to tha data about exchanges between two countries in the market :
-	exchange[1]= us -> canada
-	exchange[1]= us -> mexico
-	exchange[1]= canada -> us
-	exchange[1]= canada -> mexico
-	exchange[1]= mexico -> us
-	exchange[1]= mexico -> canada
+	Each component of the exchange array corresponds to tha data about total exports between two countries in the market :
+	exchange[1][1]= us -> us =0
+	exchange[1][2]= us -> canada
+	exchange[1][3]= us -> mexico
 	
-	Inside array<double, 2> :
-	First component of the array is amount of exchanges in billions
-	Second component of the array is percentage of total exchanges between the two countries */
-	
-	std::array<std::array<double, 2>, 6> exchange;	
+	exchange[2][1]= canada -> us
+	exchange[2][2]= canada -> canada =0
+	exchange[2][3]= canada -> mexico
+
+	exchange[3][1]= mexico -> us
+	exchange[3][2]= mexico -> canada 
+	exchange[3][3]= mexico -> mexico =0	*/	
+	std::array<double, 6> exchange;	
 };
 
 ///\class exportsnetwork
 ///\brief contains data about total exports between countries
 class exportsnetwork {
+	/*
+	exchange[1][1]= us -> us =0
+	exchange[1][2]= us -> canada
+	exchange[1][3]= us -> mexico
+	
+	exchange[2][1]= canada -> us
+	exchange[2][2]= canada -> canada =0
+	exchange[2][3]= canada -> mexico
+
+	exchange[3][1]= mexico -> us
+	exchange[3][2]= mexico -> canada 
+	exchange[3][3]= mexico -> mexico =0
+	*/
 	public:
 	std::array<std::array<double, 3>, 3> exports;
 };
