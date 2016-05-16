b438bbc49e9fd4c51797ab49f3c03c57105ae9d4
Mylène_Feuillade <mylene.feuillade@sciencespo.fr>
2016-05-16 14:30:30 +0200

    Added functions' signature to the header

--- headers/updatedata.h
+++ headers/updatedata.h
@@ -1,1 +1,7 @@
 void update(country* country1, country* country2, country* country3, market* market1, market* market2, market* market3, exportsnetwork* mynetwork);
+
+void updatecountry(country* mycountry);
+
+void updatemarket(market* mymarket);
+
+void updateexports(exportsnetwork* mynetwork);
