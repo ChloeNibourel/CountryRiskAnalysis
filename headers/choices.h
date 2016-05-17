464860284425c04977f7e2cad86903806d3fea88
Chloe Nibourel <chloe.nibourel@sciencespo.fr>
2016-05-17 18:38:43 +0200

    documentation

--- headers/choices.h
+++ headers/choices.h
@@ -1,1 +1,7 @@
+/// \file choices.h
+/// \brief headers to define choices functions for the user
+
+
+/// \fn choicecountry
+/// \brief general function that let you choose between the 3 countries
 int choicecountry(country* country1, country* country2, country* country3);
