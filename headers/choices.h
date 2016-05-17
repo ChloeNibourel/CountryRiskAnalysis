256d0ec0c319920d162c2dd380632ab493425905
Chloe Nibourel <chloe.nibourel@sciencespo.fr>
2016-05-17 19:51:08 +0200

    documentation

--- headers/choices.h
+++ headers/choices.h
@@ -7,6 +7,6 @@
 int choicecountry(country* country1, country* country2, country* country3);
 
 
-///\func percentageshock
+///\fn percentageshock
 ///\brief function that asks the user to enter the percentage
 double percentageshock();
