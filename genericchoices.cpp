c86285dac5c2f3cc7e4a21dd21bb5d3a2da01893
Chloe Nibourel <chloe.nibourel@sciencespo.fr>
2016-05-17 10:05:57 +0200

    color

--- genericchoices.cpp
+++ genericchoices.cpp
@@ -5,22 +5,23 @@
 #include "headers/colors.h"
 #include "headers/country.h"
 
-///\func int choicecountry
-///\brief general function that lets you choose between the 3 coutries
-int choicecountry(country* country1, country* country2, country* country3){
-	//give the options
 
 	//colors
     Color::Modifier blue(Color::FG_BLUE);
     Color::Modifier red(Color::FG_RED);
     Color::Modifier def(Color::FG_DEFAULT);
 
+///\func int choicecountry
+///\brief general function that lets you choose between the 3 coutries
+int choicecountry(country* country1, country* country2, country* country3){
+	//give the options
+
 
 	int choice;
 	std::cout << blue << " 1. " << (*country1).name << std::endl;
 	std::cout << " 2. "<< (*country2).name << std::endl;
 	std::cout << " 3. "<< (*country3).name << std::endl;
-	std::cout << " 4. Cancel" << std::endl;
+	std::cout << " 4. Cancel" << def << std::endl;
 		
 	std::cin >> choice;
 	
