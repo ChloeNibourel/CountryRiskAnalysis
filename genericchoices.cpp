54c94d8567095ac99c8484600f0d288f44d9c197
Mylène_Feuillade <mylene.feuillade@sciencespo.fr>
2016-05-17 18:41:37 +0200

    Added to the choicecountry function the case of an invalid choice, changed other functions accordingly

--- genericchoices.cpp
+++ genericchoices.cpp
@@ -33,6 +33,10 @@ int choicecountry(country* country1, country* country2, country* country3){
 	if (choice==1 || choice ==2 || choice==3 || choice==4){
 		return(choice);
 	}
+	
+	if (choice==0) {
+		std::cout << red << "Error : please choose 1, 2, 3 or 4" << def << std::endl;
+	}
 	else {return(0);}
 
 }
