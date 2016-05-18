88a97d6d81b9757f82eef0687a97e025bbc9bcf9
Marie.Cases <marie.cases@sciencespo.fr>
2016-05-18 13:36:01 +0200

    Fixed the documentation

--- headers/simulate.h
+++ headers/simulate.h
@@ -4,44 +4,45 @@
 
 /// \fn choicesimulate
 /// \brief gives the user different options of simulation: shock on GDP, exchange rate...
+
 void choicesimulate(country* country1, country* country2, country* country3, market* market1, market* market2, market* market3, exportsnetwork* mynetwork);
 
 /// \fn ProductionShock
-/// \brief Determine the impact of a market shock
-/// \bug Would using uscan/usmex change the marketvalue or the new value? We almost need markets to be a class defined to a country for this to work.
+/// \brief Determine the impact of a production shock to one of three markets. 
 
-/// We want to make a qualitative judgement on whether a change in one country's market will have a large or small impact on that country. We want to take as inputs: country, the effected market, and the dollar change in the market in billions. Basically, if the marketchange is negative, the country will export less and import more, thus corresponding to a decrease in that country. Since prices will likely increase in the domestic market due to a shock in the supply, its trading partners will likely demand more of their domestic market's goods => an increase in income => increase demand for imports. 
-void ProductionShock(  country* mycountry,   country* foreigncountry1,   country* foreigncountry2, int choice, int choicea, int choiceb,   market* mymarket, double change,   exportsnetwork* nafta);
+/// Once the user has chosen production shock, he is asked about the impacted country, the market (food, fuel, or machinery), and the degree of the shock. Given his choices, the program generates the inputs for the ProductionShock function. The function outputs the effect on the domestic market and on its two trading partners' markets.
+
+void ProductionShock(country* mycountry,   country* foreigncountry1,   country* foreigncountry2, int choice, int choicea, int choiceb,   market* mymarket, double change, exportsnetwork* nafta);
 
 /// \fn GDPShockImpact
 /// \brief Determines the impact of a shock to GDP growth
-/// \bug Inputting -3 will end program.
 
-/// The GDPShockImpact takes as inputs country and a variable representing the shock to GDP called GDPChange := Percent change in GDP. If we see growth (GDPChange > 0) in a country, it will increase the demand for its imports. Thus we can expect its primary trading partners to increase the volume of their exports. If rather, we see a recession (GDPChange < 0) in a country, it will decrease the demand of their imports. Thus there is a risk for its primary trading partners' exports. 
+/// Once the user has chosen the GDP Shock option, he is asked about the impacted country and the degree of the shcok. Given his choices, the program generates the inputs for the GDPShockImpact function. The function outputs the effect on the domestic country and the effect on foreign exports to the domestic country.
+
 void GDPShockImpact(country* mycountry, country* foreigncountry1, country* foreigncountry2, int choice, int choicea, int choiceb,  market* market1, market* market2, market* market3, double change, exportsnetwork* nafta);
 
 
 /// \fn ExchangeRateShockImpact
-/// \brief Impact of an exchange rate appreciation or depreciation on imports exports and GDP
-/// \bug No matter the input, it either causes a big recession or good growth levels. Could be caused by our random elasticity values. 
+/// \brief Determines the impact of an exchange rate appreciation or depreciation on imports exports and GDP
 
-/// ExchangeRate of country C will always refer to how many foreign currencies country C can buy with one unit of country C's currency (ex: If Pound is domestic and Euro foreign, and 1 Pound = 1.27 Euro, then the ExchangeRate is 1.27). Thus an appreciation relative to another currency means an increase in ExchangeRate. 
+/// Once the user has chosen the Exchange Rate shock option, he is asked about the impacted countries and the degree of the shock. The appropriate inputs for the ExchangeRateShockImpact function are generated from his answers. The function outputs the effect on the imports and exports between these two countries and specifies which markets will be most affected.
 
-/// The function takes as inputs a country and a shock to the exchange rate. An appreciation (ExchangeRate > 0) in the currency makes imports less expensive thus increasing the demand for imports while making exports more expensive thus decreasing the foreign for the domestic country's exports. The opposite for a Depreciation (ExchangeRate <= 0).  We can calculate a prediction of the change in imports and exports using the elasticity of imports and exports. Using this we can calculate a prediction for the change in the domestic country's income. If the function is called, it will output the impact on trade and will generate the GDPShockImpact using the predicted income change.
 void ExchangeRateShockImpact(country* domesticcountry, country* foreigncountry, int choice1, int choice2, market* market1, market* market2, market* market3, double change, exportsnetwork* nafta);
 
 
 /// \fn InterestRateShockImpact
-/// \brief Determine impact of a shock to interest rates. 
+/// \brief Determines the impact of a shock to domestic interest rates 
 
-/// When there is an increase in interest rates, we see an increase in the domestic exchange rate since their is more demand for the domestic currency due to superior investment returns in that country. Thus an increase in interest rates will result in a currency appreciation and a decrease in interest rates will result in a currency depreciation. The function takes as inputs the country and the interest rate change, and outputs the effect on the exchange rate while calling the ExchangeRateShockImpact function. 
+/// Once the user has chosen the Interest Rate shock option, he is asked about the impacted country and the degree of the shock. The appropriate inputs for the InterestRateShockImpact function are generated from his answers. The function outputs the effect on the exchange rate and thus the effect on the imports and exports between his two trading partners (specifying which markets will be most affected). 
+
 void InterestRateShockImpact(country* domesticcountry,   country* foreigncountry1,   country* foreigncountry2, int choice, int choicea, int choiceb, market* market1, market* market2, market* market3, double change, exportsnetwork* nafta);
 
 
 /// \fn InflationRateShockImpact
-/// \brief Determine the impact of a shock of the inflation rate.
+/// \brief Determines the impact of a shock of the inflation rate.
 
-/// When there is an increase in inflation, it results in an increase in the domestic interest rate since banks run a higher risk when they lend money. When there is a decrease in inflation we see a decrease in interest rates. The function takes an inputs the country and the inflation rate change, and outputs the effect on interest rates while calling the InterestRateShockImpact function.
+/// Once the user has chosen the Inflation Rate shock option, he is asked about the impacted country and the degree of the shock. The appropriate inputs for the InflationRateShockImpact function are generated from his answers. The function outputs the effect on the interests rates, the exchange rates, and thus the effect on the imports and exports between its two trading partners (specifying which markets will be most affected). 
+
 void InflationRateShockImpact(country* mycountry,   country* foreigncountry1,   country* foreigncountry2, int choice, int choicea, int choiceb, market* market1, market* market2, market* market3, double change,   exportsnetwork* nafta);
 
 
