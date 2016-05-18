6f7c2ee77e677ee678df6cfd8a981b2ac12b14de
Marie.Cases <marie.cases@sciencespo.fr>
2016-05-18 13:46:15 +0200

    Added documentation

--- simulate/GDPShockImpact.cpp
+++ simulate/GDPShockImpact.cpp
@@ -5,22 +5,19 @@
 #include "../headers/market.h"
 #include "../headers/simulate.h"
 
-
-
-/// \fn GDPShockImpact
-/// \brief Determines the impact of a shock to GDP growth
-
-/// \Detail The GDPShockImpact takes as inputs country and a variable representing the shock to GDP called GDPChange := Percent change in GDP. If we see growth (GDPChange > 0) in a country, it will increase the demand for its imports. Thus we can expect its primary trading partners to increase the volume of their exports. If rather, we see a recession (GDPChange < 0) in a country, it will decrease the demand of their imports. Thus there is a risk for its primary trading partners' exports. 
+/// \file GDPShockImpact.cpp
+/// \brief Defines the function GDPShockImpact 
 
 void GDPShockImpact(country* mycountry, country* foreigncountry1, country* foreigncountry2, int choice, int choicea, int choiceb,  market* market1, market* market2, market* market3, double change, exportsnetwork* nafta) {
-	market* mymarketfc1;
-	market* mymarketfc2;
-	double pmkt1expfc1 = (*market1).exchange[choicea-1][choice-1]/(*nafta).exports[choicea-1][choice-1];
-	double pmkt2expfc1 = (*market2).exchange[choicea-1][choice-1]/(*nafta).exports[choicea-1][choice-1];
-	double pmkt3expfc1 = (*market3).exchange[choicea-1][choice-1]/(*nafta).exports[choicea-1][choice-1];
-	double pmkt1expfc2 = (*market1).exchange[choiceb-1][choice-1]/(*nafta).exports[choiceb-1][choice-1];
-	double pmkt2expfc2 = (*market2).exchange[choiceb-1][choice-1]/(*nafta).exports[choiceb-1][choice-1];
-	double pmkt3expfc2 = (*market3).exchange[choiceb-1][choice-1]/(*nafta).exports[choiceb-1][choice-1];
+	market* mymarketfc1; //defines the largest export market from foreigncountry1 to mycountry
+	market* mymarketfc2; //defines the largest export market from foreigncountry2 to mycountry
+	double pmkt1expfc1 = (*market1).exchange[choicea-1][choice-1]/(*nafta).exports[choicea-1][choice-1]; //market1- fc1-mycountry
+	double pmkt2expfc1 = (*market2).exchange[choicea-1][choice-1]/(*nafta).exports[choicea-1][choice-1]; //market2
+	double pmkt3expfc1 = (*market3).exchange[choicea-1][choice-1]/(*nafta).exports[choicea-1][choice-1]; //market3
+	double pmkt1expfc2 = (*market1).exchange[choiceb-1][choice-1]/(*nafta).exports[choiceb-1][choice-1]; //market1- fc2-mycountry
+	double pmkt2expfc2 = (*market2).exchange[choiceb-1][choice-1]/(*nafta).exports[choiceb-1][choice-1]; //market2
+	double pmkt3expfc2 = (*market3).exchange[choiceb-1][choice-1]/(*nafta).exports[choiceb-1][choice-1]; //market3
+	//Determines the largest export market from foreigncountry1 to mycountry	
 	if ((pmkt1expfc1 >= pmkt2expfc1) && (pmkt1expfc1 >= pmkt3expfc1)) {
 		mymarketfc1 = market1;
 	}
@@ -30,6 +27,7 @@ void GDPShockImpact(country* mycountry, country* foreigncountry1, country* forei
 	else {
 		mymarketfc1 = market3;
 	}
+	//Determines the largest export market from foreigncountry2 to mycountry
 	if ((pmkt1expfc2 >= pmkt2expfc2) && (pmkt1expfc2 >= pmkt3expfc2)) {
 		mymarketfc2 = market1;
 	}
@@ -39,6 +37,7 @@ void GDPShockImpact(country* mycountry, country* foreigncountry1, country* forei
 	else {
 		mymarketfc2 = market3;
 	}
+	//Outputs different things depending on the shock level
 	if (change > 2) {
 		std::cout << "Good growth levels for "<< (*mycountry).name << "'s economy" << std::endl;
 		std::cout << "Low risk for " << (*foreigncountry1).name << " and " << (*foreigncountry2).name << "'s exports to " <<  			(*mycountry).name << std::endl;
