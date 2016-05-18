c0c58bf97eb4d1fcec02ee881637f938bcb66f2d
Chloe Nibourel <chloe.nibourel@sciencespo.fr>
2016-05-18 10:10:02 +0200

    documentation to simulate.h

--- simulate/country.cpp
+++ simulate/country.cpp
@@ -9,23 +9,16 @@
 /// \brief Macro level functions for country class
 /// \bug If there is a problem, it probably has to do with inputting classes. Change the inputs as the address of the class.
 
-/// \Detail The country class has functions that allow us to determine the impact of four types of macroeconomic shock: 1. Shock to GDP. 2. Shock to Exchange Rates. 3. Shock to Domestic Interest Rates. 4. Shock to Domestic Inflation Rates. We also include a function that calculates GDP.  
+/// The country class has functions that allow us to determine the impact of four types of macroeconomic shock: 1. Shock to GDP. 2. Shock to Exchange Rates. 3. Shock to Domestic Interest Rates. 4. Shock to Domestic Inflation Rates. We also include a function that calculates GDP.  
 
-/// \fn GDP 
-/// \brief Calculates GDP
-
-/// \Detail This function calculates a countries GDP as a function of domestic consuption, total exports and total imports. The variable A = country.income - (country.totalexports - country.totalimports). In other words, A is the GDP when we exclude the trade balance. A will not change in our model unless it is updated.  
+  
 
 double GDP(country* mycountry, float nexports, float nimports) {
 	double GDP = (*mycountry).A + nexports - nimports;	
 	return GDP;
 	}
 
-/// \func GDPShockImpact
-/// \brief Determines the impact of a shock to GDP growth
-/// \bug Inputting -3 will end program.
 
-/// \Detail The GDPShockImpact takes as inputs country and a variable representing the shock to GDP called GDPChange := Percent change in GDP. If we see growth (GDPChange > 0) in a country, it will increase the demand for its imports. Thus we can expect its primary trading partners to increase the volume of their exports. If rather, we see a recession (GDPChange < 0) in a country, it will decrease the demand of their imports. Thus there is a risk for its primary trading partners' exports. 
 
 void GDPShockImpact(country* mycountry, double change) {
 	if (change > 2) {
@@ -49,33 +42,22 @@ void GDPShockImpact(country* mycountry, double change) {
 		std::cout << "Do not invest in " << (*mycountry).name << "'s import markets."; 
 	}
 }
-
-/// \fn ExchangeRateShockImpact
-/// \brief Impact of an exchange rate appreciation or depreciation on imports exports and GDP
-/// \bug No matter the input, it either causes a big recession or good growth levels. Could be caused by our random elasticity values. 
-
-/// \Details ExchangeRate of country C will always refer to how many foreign currencies country C can buy with one unit of country C's currency (ex: If Pound is domestic and Euro foreign, and 1 Pound = 1.27 Euro, then the ExchangeRate is 1.27). Thus an appreciation relative to another currency means an increase in ExchangeRate. 
-
-/// The function takes as inputs a country and a shock to the exchange rate. An appreciation (ExchangeRate > 0) in the currency makes imports less expensive thus increasing the demand for imports while making exports more expensive thus decreasing the foreign for the domestic country's exports. The opposite for a Depreciation (ExchangeRate <= 0).  We can calculate a prediction of the change in imports and exports using the elasticity of imports and exports. Using this we can calculate a prediction for the change in the domestic country's income. If the function is called, it will output the impact on trade and will generate the GDPShockImpact using the predicted income change. 
+ 
 
 void ExchangeRateShockImpact(country* domesticcountry,   country* foreigncountry, int choice1, int choice2, double change,   exportsnetwork* nafta) {	
-	if (change > 0) { ///Appreciation		
+	if (change > 0) { //Appreciation		
 		std::cout << "Good for " << (*domesticcountry).name << "'s imports."<< std::endl;
 		std::cout << "Risk for " << (*domesticcountry).name << 	"'s primary exports." << std::endl;
 	}
 	if (change == 0) {
 		std::cout << "No impact on economy." << std::endl;	
 	}
-	if (change <= 0) { ///Depreciation
+	if (change <= 0) { //Depreciation
 		std::cout << "Risk for " << (*domesticcountry).name << "'s imports." << std::endl; 
 		std::cout << "Good for " << (*domesticcountry).name << "'s primary exports." << std::endl;
 	}
 }
 
-/// \fn InterestRateShockImpact
-/// \brief Determine impact of a shock to interest rates. 
-
-/// \Detail When there is an increase in interest rates, we see an increase in the domestic exchange rate since their is more demand for the domestic currency due to superior investment returns in that country. Thus an increase in interest rates will result in a currency appreciation and a decrease in interest rates will result in a currency depreciation. The function takes as inputs the country and the interest rate change, and outputs the effect on the exchange rate while calling the ExchangeRateShockImpact function. 
 
 void InterestRateShockImpact(  country* domesticcountry,   country* foreigncountry1,   country* foreigncountry2, int choice, int choicea, int choiceb, double change,   exportsnetwork* nafta) {
 	double ExchRChange = change;	
@@ -98,11 +80,6 @@ void InterestRateShockImpact(  country* domesticcountry,   country* foreigncount
 	}
 }
 
-/// \func InflationRateShockImpact
-/// \brief Determine the impact of a shock of the inflation rate.
-
-
-/// \Detail When there is an increase in inflation, it results in an increase in the domestic interest rate since banks run a higher risk when they lend money. When there is a decrease in inflation we see a decrease in interest rates. The function takes an inputs the country and the inflation rate change, and outputs the effect on interest rates while calling the InterestRateShockImpact function.
 
 void InflationRateShockImpact(  country* mycountry,   country* foreigncountry1,   country* foreigncountry2, int choice, int choicea, int choiceb, double change,   exportsnetwork* nafta) {
 	double IntRChange = change;
